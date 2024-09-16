db8caa7389c163b770c243468238d261a149ad3c
RajasekharY-9 <yalamillirajasekhar123@gmail.com>
2024-09-16 16:36:27 +0530

    setup

---
+++ SIRI-FASHIONS/src/main/java/com/siri/SiriFashionsApplication.java
@@ -0,0 +1,13 @@
+package com.siri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiriFashionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiriFashionsApplication.class, args);
+	}
+
+}
